c847b182167a4a4ca71b13df45fe19ad2eb9f25c
andrey <andreytarasyuk>
2018-04-12 11:55:57 +0300

    amended popular block

--- build/js/app.js
+++ build/js/app.js
@@ -1,9 +1,6 @@
 console.log('common');
 
 $(document).ready(function() {
-	$('a[href]').click(function (e) {
-		e.preventDefault();
-	});
 
 	$('.js-slider-preview').slick({
 		slidesToShow: 1,
@@ -12,6 +9,25 @@ $(document).ready(function() {
 		fade: true,
 		dots: true,
 		arrows: false
+	});
+
+	$('.js-scroll-to').on("click", function(e){
+		e.preventDefault();
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 1000);
+	});
+	// transparent input, etx
+	$('input, textarea').each(function(){
+		var placeholder = $(this).attr('placeholder');
+
+		$(this).focus(function(){
+			$(this).attr('placeholder', '');
+		});
+		$(this).blur(function(){
+			$(this).attr('placeholder', placeholder);
+		});
 	});
 
 });
