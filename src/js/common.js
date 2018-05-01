6587863a178303543faf6ec9019b00c7d2402055
whats0n <mrdvv@mail.ru>
2018-05-01 22:26:49 +0300

    add posponsive add script

--- src/js/common.js
+++ src/js/common.js
@@ -30,6 +30,214 @@ $(document).ready(function() {
 		appendDots:$('.js-slick-dots'),
 		arrows: false
 	});
+	$('.js-slider-reviews').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		arrows: false,
+		responsive: [
+		{
+			breakpoint: 9999,
+			settings: 'unslick'
+		},
+		{
+			breakpoint: 769,
+			settings: {
+				dots: true,
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
+	$('.js-slider-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.js-slider-nav',
+		responsive: [
+		{
+			breakpoint: 1120,
+			settings: {
+				dots: true
+			} 
+		}
+		]
+	});
+	$('.js-slider-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		vertical: true,
+		focusOnSelect: true,
+		arrows: false,
+		asNavFor: '.js-slider-for'
+	});
+
+	// accordion
+	$('.js-accordion').each((i, current) => {
+		current = $(current);
+		const currentBtn = current.find('.js-accordion-btn');
+		const currentItem = current.find('.js-accordion-item');
+		const active = 'is-active';
+
+		if(!current.hasClass(active)) {
+			currentItem.slideUp();
+		}
+
+		currentBtn.click(function(e) {
+			e.preventDefault();
+			current.toggleClass(active);
+			currentBtn.toggleClass(active);
+			currentItem.slideToggle();
+		});		
+	});
+	// filter
+	$('.js-filter-btn').click(function(e) {
+		e.preventDefault();
+		$('.js-filter-item').slideToggle();
+	});
+	// popup
+	const OPEN = 'is-open';
+	$('.js-modal-control').each((i, control) => {
+		control = $(control);
+		const modal = $(`.js-modal[data-modal="${control.data('target')}"]`);
+		const prevent = modal.find('.js-modal-prevent');
+
+		control.on('click', e => {
+			e.preventDefault();
+			modal.toggleClass(OPEN);
+		});
+
+		modal.on('click', e => {
+			if ($(e.target).closest(prevent).length) return;
+			modal.removeClass(OPEN);			
+		});
+	});
+	// select2
+	const select = $('.js-select');
+	const selectInModal = select.filter((i, s) => !!$(s).closest('.js-modal').length);
+	const selectNotInModal = select.not(selectInModal);
+	console.log(selectNotInModal);
+	console.log(selectInModal);
+
+	selectInModal.each((i, s) => {
+		s = $(s);
+		s.select2({
+			minimumResultsForSearch: -1,
+			dropdownParent: s.parent()
+		});
+	});
+
+	selectNotInModal.select2({
+		minimumResultsForSearch: -1
+	});
+
+	const showCurrentTab = props => {
+		const { controls, control, contents, content, i, prev, next } = props;
+		const ACTIVE = 'active';
+		const HIDDEN = 'hidden';
+
+		controls
+			.add(contents)
+			.removeClass(ACTIVE);
+		control
+			.add(content)
+			.addClass(ACTIVE);
+
+		if (i <= 0) prev.addClass(HIDDEN);
+		else prev.removeClass(HIDDEN);
+
+		if (i >= controls.length - 1) next.addClass(HIDDEN)
+		else next.removeClass(HIDDEN);
+	};
+
+	$('.js-tabs').each((i, container) => {
+		
+		container = $(container);
+		const controls = container.find('.js-tabs-control');
+		const contents = container.find('.js-tabs-content');
+		const prev = container.find('.js-tabs-prev');
+		const next = container.find('.js-tabs-next');
+		const ACTIVE = 'active';
+
+		controls.each((i, control) => {
+			control = $(control);
+			const target = control.data('tab');
+			const content = contents.filter(`[data-content="${target}"]`);
+
+			if (control.hasClass(ACTIVE)) showCurrentTab({
+				controls,
+				control,
+				contents,
+				content,
+				i,
+				prev,
+				next
+			});
+
+			control.click(e => {
+				e.preventDefault();
+				if (control.hasClass(ACTIVE)) return;
+				showCurrentTab({
+					controls,
+					control,
+					contents,
+					content,
+					i,
+					prev,
+					next
+				});
+			});
+		});
+
+		prev.click(e => {
+			e.preventDefault();
+			const current = controls.filter(`.${ACTIVE}`);
+			const control = current.prev();
+			if (!control.length) return;
+			const target = control.data('tab');
+			const content = contents.filter(`[data-content="${target}"]`);
+			let i = 0;
+			controls.each((index, c) => {
+				if (c === control.get(0)) i = index;
+			});
+
+			showCurrentTab({
+				controls,
+				control,
+				contents,
+				content,
+				i,
+				prev,
+				next
+			});
+		});
+
+		next.click(e => {
+			e.preventDefault();
+			const current = controls.filter(`.${ACTIVE}`);
+			const control = current.next();
+			if (!control.length) return;
+			const target = control.data('tab');
+			const content = contents.filter(`[data-content="${target}"]`);
+			let i = 0;
+			controls.each((index, c) => {
+				if (c === control.get(0)) i = index;
+			});
+
+			showCurrentTab({
+				controls,
+				control,
+				contents,
+				content,
+				i,
+				prev,
+				next
+			});
+		});
+
+	});
 
 });
 
